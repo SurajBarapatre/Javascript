0ca7082330352ca9398ce4534bd8d00fe560073a
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-08-05 23:03:46 +0530

    Add files via upload

---
+++ practice.3/script.js
@@ -0,0 +1,10 @@
+// Declare Celsius temperature
+let celsius = 150;
+
+// Convert Celsius to Fahrenheit
+let fahrenheit = (celsius * 9 / 5) + 32;
+
+// Display Output
+document.writeln("<h2>Temperature Converter</h2>");
+document.writeln("Temperature in Celsius = " + celsius + "°C <br>");
+document.writeln("Temperature in Fahrenheit = " + fahrenheit + "°F");
