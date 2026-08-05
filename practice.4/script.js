c778a4f86437f065ff7b5dfa15aebcdbd29570c0
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-08-05 23:04:40 +0530

    Add files via upload

---
+++ practice.4/script.js
@@ -0,0 +1,10 @@
+// Declare meter value
+let meter = 25000;
+
+// Convert meter to kilometer
+let kilometer = meter / 1000;
+
+// Display output
+document.writeln("<h2>Meter to Kilometer Converter</h2>");
+document.writeln("Meter = " + meter + " m <br>");
+document.writeln("Kilometer = " + kilometer + " km");
