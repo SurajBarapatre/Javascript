081af96dab8409629c057004e1277d97650db029
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-08-07 23:05:21 +0530

    Add files via upload

---
+++ practice.11/script.js
@@ -0,0 +1,35 @@
+// WAP to Use All Relational Operators
+
+let a = 10;
+let b = 20;
+let c = "30";
+
+console.log("Value of a =", a);
+console.log("Value of b =", b);
+console.log("Value of c =", c);
+
+console.log("All Relational Operators in JavaScript:");
+
+// Greater Than Comparison //
+console.log("a > b =", a > b);
+
+// Less Than Comparison //
+console.log("a < b =", a < b);
+
+// Greater Than or Equal To Comparison //
+console.log("a >= b =", a >= b);
+
+// Less Than or Equal To Comparison //
+console.log("a <= b =", a <= b);
+
+// Equal To (Value) Comparison //
+console.log("a == c =", a == c);
+
+// Strict Equal (Value + Data Type) Comparison //
+console.log("a === c =", a === c);
+
+// Not Equal (Value) Comparison //
+console.log("a != b =", a != b);
+
+// Strict Not Equal (Value + Data Type) Comparison //
+console.log("a !== c =", a !== c);
