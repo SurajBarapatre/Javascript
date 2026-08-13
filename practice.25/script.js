8514762e751c34d0c3821c88dfb9c0c2a2c67ff8
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-08-13 22:30:56 +0530

    Update script.js

--- practice.25/script.js
+++ practice.25/script.js
@@ -1,12 +1,10 @@
 // wap to print leap years from 2000 to 3000 using while loop.
 
-let year = 2000;
+let startYear = 2000;
 
-while (year <= 3000) 
-    {
-    if ((year % 4 == 0 && year % 100 !== 0) || (year % 400 == 0)) 
-    {
-        console.log(year);
-    }
-    year++;
+while(startYear <= 3000)
+{
+   console.log(startYear)
+
+    startYear+=4;
 }
