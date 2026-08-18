678d8dbc2598f76c7e471f7c7c6f13439c302982
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-08-19 00:11:43 +0530

    Add files via upload

---
+++ Practice.51/script.js
@@ -0,0 +1,34 @@
+// WAP to Use All Relational Operators by Differnt Comparisons using function.
+
+function relationalOperators(a, b, c)
+{
+    console.log("Value of a =", a);
+    console.log("Value of b =", b);
+    console.log("Value of c =", c);
+
+    console.log("All Relational Operators in JavaScript:");
+
+    // Greater Than Comparison
+    console.log("a > b =", a > b);
+
+    // Less Than Comparison
+    console.log("a < b =", a < b);
+
+    // Greater Than or Equal To Comparison
+    console.log("a >= b =", a >= b);
+
+    // Less Than or Equal To Comparison
+    console.log("a <= b =", a <= b);
+
+    // Equal To (Value) Comparison
+    console.log("a == c =", a == c);
+
+    // Not Equal (Value) Comparison
+    console.log("a != b =", a != b);
+
+    // Strict Not Equal (Value + Data Type) Comparison
+    console.log("a !== c =", a !== c);
+}
+
+// Function Call
+relationalOperators(10, 20, 30);
