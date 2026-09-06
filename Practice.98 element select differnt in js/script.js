589898b6517cee6a6be3f507f13dc14b134463dc
Suraj Barapatre <surajbarapatre007.edu@gmail.com>
2026-09-06 21:58:57 +0530

    Update script.js

--- Practice.98 element select differnt in js/script.js
+++ Practice.98 element select differnt in js/script.js
@@ -1,4 +1,5 @@
 // how to select an element by id,class,tag by only one function
+
 function selectElement(selector) {                                  // this function takes a selector as an argument and returns the corresponding element(s)
     if (selector.startsWith('#')) {                                 // if the selector starts with a '#' it means we are selecting an element by id
         return document.getElementById(selector.slice(1));          // we use slice(1) to remove the '#' from the selector
